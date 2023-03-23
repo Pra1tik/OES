46995f552e1c9f8950da008fea64bbb9bcdc5ade
Pra1tik <pratikhadkhale@gmail.com>
2023-03-23 23:28:08 +0545

    Timer added

--- onlineExam/stream/urls.py
+++ onlineExam/stream/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('lobby/', views.lobby, name="lobby"),
+    path('tlobby/', views.tlobby, name="tlobby"),
+
     path('room/', views.room),
     path('get_token/', views.getToken),
 
